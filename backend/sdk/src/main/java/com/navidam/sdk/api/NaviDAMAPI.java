f3d84f585766d50f178308f3527962cc19523627
farzia <ghaforyfarzia@gmail.com>
2025-12-18 10:32:03 +0100

    New

--- backend/sdk/src/main/java/com/navidam/sdk/api/NaviDAMAPI.java
+++ backend/sdk/src/main/java/com/navidam/sdk/api/NaviDAMAPI.java
@@ -1,30 +1,42 @@
 package com.navidam.sdk.api;
 
-import com.navidam.sdk.internal.html.Plantilla;
 import com.navidam.sdk.internal.melodia.Melodia;
+import com.navidam.sdk.internal.melodia.Nota;
+import com.navidam.sdk.internal.melodia.Figura;
+import com.navidam.sdk.internal.melodia.Instrumento;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NaviDAMAPI {
 
-    private static NaviDAMAPI instace;
+    private static NaviDAMAPI instance;
 
     public static NaviDAMAPI getInstance() {
-        if (instace == null) {
-            instace = new NaviDAMAPI();
+        if (instance == null) {
+            instance = new NaviDAMAPI();
         }
-        return instace;
+        return instance;
     }
 
     public List<Melodia> listadoMelodias() {
-        return List.of(
-                // LaVacaLola.crear()
-        );
-    }
+        List<Melodia> melodias = new ArrayList<>();
 
-    public List<Plantilla> listadoPlantillas() {
-        return List.of(
-                // new Plantilla("demo").path("demo.html")
-        );
+        // Mine melody
+        Melodia demo = new Melodia("Demo Melody")
+            .instrumento(Instrumento.PIANO_ACUSTICO)
+            .nota(Nota.DO, Figura.NEGRA)
+            .nota(Nota.MI, Figura.CORCHEA)
+            .nota(Nota.SOL, Figura.CORCHEA)
+            .nota(Nota.FA, Figura.NEGRA)
+            .nota(Nota.RE, Figura.BLANCA);
+
+        melodias.add(demo);
+
+        // NAin y Andress:
+        // melodias.add(new MelodiaEquipo2()...);
+        // melodias.add(new MelodiaEquipo3()...);
+
+        return melodias;
     }
 }
--- backend/sdk/src/main/java/com/navidam/sdk/api/melodias/Prueba.java
+++ backend/sdk/src/main/java/com/navidam/sdk/api/melodias/Prueba.java
@@ -9,13 +9,26 @@ public class Prueba {
     public static void main(String[] args) {
         try {
             new Melodia("Demo Melody")
-                .instrumento(Instrumento.PIANO_ACUSTICO)
-                .nota(Nota.DO, Figura.NEGRA)
-                .nota(Nota.MI, Figura.CORCHEA)
-                .nota(Nota.SOL, Figura.CORCHEA)
-                .nota(Nota.FA, Figura.NEGRA)
-                .nota(Nota.RE, Figura.BLANCA)
-                .escuchar();
+            .nota(Nota.LA, Figura.NEGRA)
+            .nota(Nota.RE, Figura.CORCHEA)
+            .nota(Nota.MI, Figura.CORCHEA)
+            .nota(Nota.FA, Figura.NEGRA)
+            .nota(Nota.MI, Figura.BLANCA)
+            .nota(Nota.LA, Figura.NEGRA)
+            .nota(Nota.RE, Figura.CORCHEA)
+            .nota(Nota.MI, Figura.CORCHEA)
+            .nota(Nota.FA, Figura.NEGRA)
+            .nota(Nota.MI, Figura.BLANCA)
+            .nota(Nota.SOL, Figura.NEGRA)
+            .nota(Nota.FA, Figura.NEGRA)
+            .nota(Nota.MI, Figura.NEGRA)
+            .nota(Nota.RE, Figura.NEGRA)
+            .nota(Nota.LA, Figura.NEGRA)
+            .nota(Nota.SOL, Figura.NEGRA)
+            .nota(Nota.FA, Figura.NEGRA)
+            .nota(Nota.MI, Figura.NEGRA)
+            .escuchar();
+                
         } catch (Exception e) {
             e.printStackTrace();
         }
