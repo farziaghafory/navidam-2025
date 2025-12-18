ece062ca11bd56a4b90aca37d005463922f04d9d
Iván <ivan.corrales.solera@gmail.com>
2025-12-18 03:02:05 +0100

    add fixes

--- backend/api/src/main/java/com/navidam/api/config/HttpConfig.java
+++ backend/api/src/main/java/com/navidam/api/config/HttpConfig.java
@@ -2,12 +2,16 @@ package com.navidam.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class HttpConfig {
     @Bean
     public RestClient.Builder restClientBuilder() {
-        return RestClient.builder();
+        SimpleClientHttpRequestFactory rf = new SimpleClientHttpRequestFactory();
+        rf.setConnectTimeout(3_000);
+        rf.setReadTimeout(10_000);
+        return RestClient.builder().requestFactory(rf);
     }
 }
